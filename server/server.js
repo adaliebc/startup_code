ffe96e762ecd6620424b4cc981e605a965e6c4ce
Adalie Crockett <adaliebc@byu.edu>
2023-12-17 13:39:46 -0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    // Broadcast the received message to all clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3001, () => {
+  console.log('WebSocket server is listening on port 3001');
+});
